e80a8e502e133fa6a7ec86d6a8ac8fea6224ce99
Astro <astro@spaceboyz.net>
2014-11-24 19:26:28 +0100

    IPv6 support

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ var cache = {}
 
 module.exports = function addrToIPPort (addr) {
   if (!cache[addr]) {
-    var s = addr.split(':')
-    cache[addr] = [ s[0], Number(s[1]) ]
+    var m, host, port
+    if ((m = addr.match(/^\[([0-9a-f:]+)\]:(\d+)$/)) ||
+        (m = addr.match(/^([0-9\.]+):(\d+)$/))) {
+      host = m[1]
+      port = Number(m[2])
+    } else {
+      throw new Error('Invalid IP/Port: ' + addr)
+    }
+    cache[addr] = [ host, port ]
   }
   return cache[addr]
 }
--- test/basic.js
+++ test/basic.js
@@ -4,6 +4,7 @@ var test = require('tape')
 test('Basic tests', function (t) {
   t.deepEqual(addrToIPPort('1.2.3.4:1000'), [ '1.2.3.4', 1000 ])
   t.deepEqual(addrToIPPort('2.3.4.5:1000'), [ '2.3.4.5', 1000 ])
+  t.deepEqual(addrToIPPort('[2a00:1450:4008:801::1011]:80'), [ '2a00:1450:4008:801::1011', 80 ])
 
   // test that cache works correctly
   var data1 = addrToIPPort('1.2.3.4:2000')
