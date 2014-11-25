c54c60710f81463e814e6bbbe20a1aaf4764e095
Astro <astro@spaceboyz.net>
2014-11-25 02:03:09 +0100

    allow non-numerical hostnames
    bittorrent-dht uses this with "router.bittorrent.com:6881"

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ module.exports = function addrToIPPort (addr) {
   if (!cache[addr]) {
     var m, host, port
     if ((m = addr.match(/^\[([0-9a-f:]+)\]:(\d+)$/)) ||
-        (m = addr.match(/^([0-9\.]+):(\d+)$/))) {
+        (m = addr.match(/^([0-9\.]+):(\d+)$/)) ||
+        (m = addr.match(/^([^:]+):(\d+)$/))) {
       host = m[1]
       port = Number(m[2])
     } else {
