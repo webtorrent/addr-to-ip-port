aad39ab073eeef03887d809d6ccf239056369c7f
Feross Aboukhadijeh <feross@feross.org>
2014-11-27 00:21:55 +0800

    perf: test retrieving from cache too

--- perf/basic.js
+++ perf/basic.js
@@ -10,7 +10,11 @@ for (var i = 1; i < 65536; i++) {
 suite
   .add('addr-to-ip-port', function () {
     addrToIPPort.reset()
+    var ipPort
     for (var i = 0, len = addrs.length; i < len; i++) {
-      var ipPort = addrToIPPort(addrs[i])
+      ipPort = addrToIPPort(addrs[i])
+    }
+    for (var i = 0, len = addrs.length; i < len; i++) {
+      ipPort = addrToIPPort(addrs[i])
     }
   })
