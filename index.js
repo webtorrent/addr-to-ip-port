6798248416bd2a34e4146d5256ae3bcb80d51cb0
Feross Aboukhadijeh <feross@feross.org>
2014-11-26 17:10:43 +0800

    IPv6 support

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var ADDR_RE = /^\[?([^\]]+)\]?:(\d+)$/ // ipv4/ipv6/hostname + port
+
 var cache = {}
 
 module.exports = function addrToIPPort (addr) {
   if (!cache[addr]) {
-    var s = addr.split(':')
-    cache[addr] = [ s[0], Number(s[1]) ]
+    var m = ADDR_RE.exec(addr)
+    cache[addr] = [ m[1], Number(m[2]) ]
   }
   return cache[addr]
 }
