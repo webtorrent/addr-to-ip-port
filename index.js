404bbfd8185b382d84ad40b403a89a11f4b82a75
Feross Aboukhadijeh <feross@feross.org>
2014-11-27 00:21:37 +0800

    reset cache when it gets to 100,000 elements
    this is more predictable than using a time-based approach

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ var ADDR_RE = /^\[?([^\]]+)\]?:(\d+)$/ // ipv4/ipv6/hostname + port
 
 var cache = {}
 
+// reset cache when it gets to 100,000 elements (~ 600KB of ipv4 addresses)
+// so it will not grow to consume all memory in long-running processes
+var size = 0
+
 module.exports = function addrToIPPort (addr) {
+  if (size === 100000) cache = {}
   if (!cache[addr]) {
     var m = ADDR_RE.exec(addr)
     if (!m) throw new Error('invalid addr')
     cache[addr] = [ m[1], Number(m[2]) ]
+    size += 1
   }
   return cache[addr]
 }
@@ -14,8 +20,3 @@ module.exports = function addrToIPPort (addr) {
 module.exports.reset = function reset () {
   cache = {}
 }
-
-// reset cache every hour so it will not grow to consume all memory
-// in long-running processes (like in server dameons)
-var interval = setInterval(module.exports.reset, 60 * 60 * 1000)
-if (interval.unref) interval.unref()
